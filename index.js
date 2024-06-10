743e77b90fc9a404b377e164b432d9fa1dd87ded
FernandaFloresVera <100867517+FernandaFloresVera@users.noreply.github.com>
2024-06-09 20:24:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('Hola mundoooo');
+    res.send('Hello world');
     });
 
 app.listen(port, () => {
